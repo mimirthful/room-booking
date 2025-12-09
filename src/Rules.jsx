56bec1caeb081958db168f8d364574798371a4aa
Alex Lankio <alex.lankio@proton.me>
2025-12-09 21:44:55 +0200

    Fix styling and grammar

--- src/Rules.jsx
+++ src/Rules.jsx
@@ -8,10 +8,8 @@ function Rules ()
     const { Language } = useContext(LanguageContext);
     return <>
     <div id="rules">
-            
             <img className="borderImg" src={borderImage} alt= "comic styled border decoration made of flowers"/>
             <h2>{Language.rulesTitle}</h2>
-            
             <ol>
                 <li> <p> {Language.rulesFirst}
                     <strong> {Language.rulesFirstBoldPart}</strong> </p>
@@ -32,15 +30,14 @@ function Rules ()
                     {Language.rulesSixth}
                 </li>
             </ol>
-            <img id="bookImg" src={bookImage} alt="Comic styled picture of an open book with flowers around it"/> <br/> 
-    <p> <strong> {Language.rulesAddendumOne} </strong> {Language.rulesAddendumTwo} </p> <br/>
-    <p> {Language.rulesAddendumThree} </p> <br/>
-    <p>  {Language.rulesAddendumFour}</p> <br/>
-    <p> <strong> {Language.rulesAddendumFive} </strong></p> <br/>
+            <img id="bookImg" src={bookImage} alt="Comic styled picture of an open book with flowers around it"/>
+    <p> <strong> {Language.rulesAddendumOne} </strong> {Language.rulesAddendumTwo} </p>
+    <p> {Language.rulesAddendumThree} </p>
+    <p>  {Language.rulesAddendumFour}</p>
+    <p> <strong> {Language.rulesAddendumFive} </strong></p>
     <img className="borderImg" src={borderImage} alt= "comic styled border decoration made of flowers" style={{ transform: 'rotate(180deg)' }} />
-    <br/>
-    <p style={{textAlign:"center"}}> {Language.rulesAddendumSix} </p> <br/>
-    <p style={{textAlign:"center"}}> <strong> {Language.rulesAddendumSeven} </strong> </p> <br/>
+    <p style={{textAlign:"center"}}> {Language.rulesAddendumSix} </p>
+    <p style={{textAlign:"center"}}> <strong> {Language.rulesAddendumSeven} </strong> </p>
 </div>
     </>
 }
